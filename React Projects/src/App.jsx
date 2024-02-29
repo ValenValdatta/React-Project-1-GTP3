d4991d5ffeceebc185b99912e7c490cfc12f9e13
Valen <valentin.valdatta.94@gmail.com>
2024-02-28 21:47:11 -0300

    primer commit del primer proyecto de react. Hice el maquetado de la pagina y empece a agregar CSS al componente NAVBAR

---
+++ React Projects/src/App.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./App.css";
+
+import Navbar from "./components/navbar/Navbar";
+import Cta from "./components/cta/Cta"
+import Brand from "./components/brand/Brand";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import Features from "./containers/features/Features";
+import Header from "./containers/header/Header";
+import Blog from "./containers/blog/Blog";
+import Footer from "./containers/footer/Footer";
+import Possibility from "./containers/possibility/Possibility";
+
+
+
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <Cta />
+      <Cta />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
